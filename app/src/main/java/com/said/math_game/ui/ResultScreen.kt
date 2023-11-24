ec6783be27c3be5029c9e880d11ce7fb72b85998
PC680 <elshodxushvaqtov07@gmailc.om>
2023-11-24 09:55:22 +0500

    a

--- app/src/main/java/com/said/math_game/ui/ResultScreen.kt
+++ app/src/main/java/com/said/math_game/ui/ResultScreen.kt
@@ -6,17 +6,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.said.math_game.util.SharedPref
+
+@Composable
 
 fun ResultScreen(score: Int, tasks: Int) {
     val context = LocalContext.current
     val top =
-        SharedPreference(context).getRecords().sortedWith(compareBy({ it.second }, { it.first }))
+        SharedPref(context).getRecords().sortedWith(compareBy({ it.second }, { it.first }))
             .asReversed()[0]
     Column(
         modifier = Modifier.fillMaxSize(),
--- app/src/main/java/com/said/math_game/util/SharedPref.kt
+++ app/src/main/java/com/said/math_game/util/SharedPref.kt
@@ -10,14 +10,24 @@ class SharedPref private constructor(context: Context) {
         private var instance: SharedPref? =null
          fun getInstance(context: Context): SharedPref? {
             if (instance == null) instance = SharedPref(context)
-            return instance
+            return instance !!
         }
     }
     fun getHighScore(level: Int){
         shared.getInt("hs$level", 0)
     }
-
+    fun getRecord(level: Int): Int{
+        return shared.getInt("record$level", 0)
+    }
+    fun getRecords(level: Int, score:Int){
+        edit.putInt("record$level", score).apply()
+    }
     fun setHighScore(level: Int, score:Int){
         edit.putInt("hs$level", score).apply()
+    }
+
+    fun getRecords(level: Int,score: Int): Any {
+        edit.
+
     }
 }
